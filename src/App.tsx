3e84e88cab7a2d402caf7f50b1384072bb406dcf
miangee21 <mianhassan5520@gmail.com>
2026-03-11 15:24:58 +0500

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-green-800 font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
